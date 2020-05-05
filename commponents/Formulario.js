b9d4eb79e2f72fb2cb59e75cd5dfe83f4995f981
Miguel Maidana <maidana.miguell@gmail.com>
2020-05-04 21:17:43 -0300

    Agregamos un Picker a nuestro formulario. Creamos  un Boton para enviar la peticion a la API

--- commponents/Formulario.js
+++ commponents/Formulario.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Text, StyleSheet, View, TextInput} from "react-native"
+import {Text, StyleSheet, View, TextInput, Picker, TouchableWithoutFeedback} from "react-native"
 
 const Formulario =() =>{
 return (
@@ -12,6 +12,23 @@ return (
                         placeholderTextColor="#666"
                     />
                 </View>
+                <View>
+                        <Picker>
+                            <Picker.Item label="--Seleccione un pais--" value=""/>
+                            <Picker.Item label="Estados Unidos" value="US"/>
+                            <Picker.Item label="Mexico" value="MX"/>
+                            <Picker.Item label="Argentina" value="AR"/>
+                            <Picker.Item label="Colombia" value="CO"/>
+                            <Picker.Item label="Costa Rica" value="CR"/>
+                            <Picker.Item label="España" value="ES"/>
+                            <Picker.Item label="Peru" value="PE"/>
+                        </Picker>
+                </View>
+                    <TouchableWithoutFeedback>
+                        <View>
+                            <Text>Buscar Clima</Text>
+                        </View>
+                    </TouchableWithoutFeedback>
         </View>
        </>
     )
